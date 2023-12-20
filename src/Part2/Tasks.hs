652567ab942ce338d3e36b77bd0e94353d2aa65d
Михаил Лихолетов <odaving111@gmail.com>
2023-12-21 02:42:11 +0300

    Part2 completed

--- src/Part2/Tasks.hs
+++ src/Part2/Tasks.hs
@@ -1,29 +1,57 @@
 module Part2.Tasks where
 
-import Util(notImplementedYet)
-
 data BinaryOp = Plus | Minus | Times deriving (Show, Eq)
 
-data Term = IntConstant { intValue :: Int }          -- числовая константа
-          | Variable    { varName :: String }        -- переменная
-          | BinaryTerm  { op :: BinaryOp, lhv :: Term, rhv :: Term } -- бинарная операция
-             deriving(Show,Eq)
+data Term
+  = IntConstant {intValue :: Int} -- числовая константа
+  | Variable {varName :: String} -- переменная
+  | BinaryTerm {op :: BinaryOp, lhv :: Term, rhv :: Term} -- бинарная операция
+  deriving (Show, Eq)
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) = notImplementedYet
+(|+|) = BinaryTerm Plus
+
+infixl 6 |+|
+
 (|-|) :: Term -> Term -> Term
-(|-|) = notImplementedYet
+(|-|) = BinaryTerm Minus
+
+infixl 6 |-|
+
 (|*|) :: Term -> Term -> Term
-(|*|) = notImplementedYet
+(|*|) = BinaryTerm Times
+
+infixl 7 |*|
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = notImplementedYet
+replaceVar varName replacement (Variable varName')
+  | varName == varName' = replacement
+  | otherwise = Variable varName'
+replaceVar varName replacement (BinaryTerm op lhv rhv) = BinaryTerm op replaceLhv replaceRhv
+  where
+    replaceVar' = replaceVar varName replacement
+    replaceLhv = replaceVar' lhv
+    replaceRhv = replaceVar' rhv
+replaceVar _ _ expression = expression
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate = notImplementedYet
+evaluate (BinaryTerm Plus (IntConstant lhv) (IntConstant rhv)) = IntConstant (lhv + rhv)
+evaluate (BinaryTerm Minus (IntConstant lhv) (IntConstant rhv)) = IntConstant (lhv - rhv)
+evaluate (BinaryTerm Times (IntConstant lhv) (IntConstant rhv)) = IntConstant (lhv * rhv)
+evaluate term@(BinaryTerm op lhv@BinaryTerm {} rhv)
+  | lhv' == lhv = term
+  | otherwise = evaluate (BinaryTerm op lhv' rhv)
+  where
+    lhv' = evaluate lhv
+evaluate term@(BinaryTerm op lhv rhv@BinaryTerm {})
+  | rhv' == rhv = term
+  | otherwise = evaluate (BinaryTerm op lhv rhv')
+  where
+    rhv' = evaluate rhv
+evaluate expression = expression
